c61d4969a989676dceb4122bc2d696875e9034e2
MiyuYokoyama0109 <miyu.y@latona.io>
2023-02-22 13:24:14 +0900

    release

--- data-platform-language-sql-language-text-data.sql
+++ data-platform-language-sql-language-text-data.sql
@@ -1,9 +1,9 @@
-CREATE TABLE `data_platform_language_text_data`
+CREATE TABLE `data_platform_language_language_text_data`
 (
     `Language`                      varchar(2) NOT NULL,
     `CorrespondingLanguage`         varchar(2) NOT NULL,
     `LanguageName`                  varchar(100) DEFAULT NULL,
     PRIMARY KEY (`Language`, `CorrespondingLanguage`)
-    CONSTRAINT `DataPlatformLanguageTextData_fk` FOREIGN KEY (`Language`) REFERENCES `data_platform_language_language_data` (`Language`)
+    CONSTRAINT `DataPlatformLanguageLanguageTextData_fk` FOREIGN KEY (`Language`) REFERENCES `data_platform_language_language_data` (`Language`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
